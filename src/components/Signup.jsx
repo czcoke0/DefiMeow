b4e7a3f434515db680e96343f38835fe668176be
cz <clairezhangxy@gmail.com>
2023-06-13 21:40:15 +0800

    icon- title update

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -11,13 +11,14 @@ const Signup = () => {
             <img src={crypto} alt=''/>
         </div>
     
-        <div className='right'></div>
+        <div className='right'>
             <h2>Earn passive income with crypto</h2>
-            <p>Earn up to 12% annual rewards on 30+ digital assets. Simply hold your assets in the app to automatically earn rewrds at the end of each month with no lockups and no limits</p>
+            <p>Earn up to 12% annual rewards on 30+ digital assets. Simply hold your assets in the app to automatically earn rewards at the end of each month with no lockups and no limits</p>
             <div className='input-container'>
                 <input type="email"  placeholder='Enter your email' />
                 <button className='btn'>Learn More</button>
-        </div>
+        	</div>
+      	</div>
       </div>
     </div>
   )
