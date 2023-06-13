f3a3a18a48e769fe7ce2a6055efd1cd8c6027363
cz <clairezhangxy@gmail.com>
2023-06-13 17:59:01 +0800

    API for showing top 6 cards based on mkt shares

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-import BTC from '../assets/btc.png'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+// import BTC from '../assets/btc.png'
 import {FiArrowUpRight, FiArrowDown} from 'react-icons/fi'
 import './Featured.css'
 const Featured = () => {
+  const [data, setData]= useState(null)
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false&locale=en'
+  useEffect(()=>{
+    axios.get(url).then((res)=>{
+      setData(res.data)
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }, [])
+  console.log(data)
+  if(!data) return null
   return (
     <div className='featured'>
       <div className='container'>
@@ -14,15 +26,29 @@ const Featured = () => {
         </div>
         {/* right side */}
         <div className='right'>
-          <div className='top'>
-            <img src={BTC} />
-          </div>
-          <div>
-            <h5>Bitcoin</h5>
-            <p>$29,000</p>
-          </div>
-          <span><FiArrowUpRight/>2.5%</span>
+          {
+            data.map((market)=> {
+              return (
+                <div className='card'>
+                  <div className='top'>
+                    <img src={market.image} alt=''/>
+                  </div>
+                  <div>
+                    <h5>{market.name}</h5>
+                    <p>${market.current_price.toLocaleString()}</p>
+                  </div>
+                  {market.price_change_percentage_24h <0?(
+                    <span className='red'><FiArrowDown className='icon'/>{market.price_change_percentage_24h.toFixed(2)}%</span>
+                    ):(
+                    <span className='green'><FiArrowUpRight className='icon'/>{market.price_change_percentage_24h.toFixed(2)}%</span>
+                  )}
+                </div>
+                );
+            })
+          }
+
         </div>
+
       </div>
       
     </div>
